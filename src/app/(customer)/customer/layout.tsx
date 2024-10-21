e0efe897fb5c95407020c4cd83398bbb07c8cc88
Quan <phamdinhquan202@gmail.com>
2024-10-21 21:51:46 +0700

    fix responsive

--- src/app/(customer)/customer/layout.tsx
+++ src/app/(customer)/customer/layout.tsx
@@ -1,34 +1,35 @@
-import { auth } from '@/auth';
-import AdminFooter from '@/components/layout/admin.footer';
-import CustomerContent from '@/components/layout/customer.content';
-import CustomerHeader from '@/components/layout/customer.header';
-import CustomerSideBar from '@/components/layout/customer.sidebar';
-import { CustomerContextProvider } from '@/library/customer.context';
+import { auth } from "@/auth";
+import AdminFooter from "@/components/layout/admin.footer";
+import CustomerContent from "@/components/layout/customer.content";
+import CustomerHeader from "@/components/layout/customer.header";
+import CustomerSideBar from "@/components/layout/customer.sidebar";
+import { CustomerContextProvider } from "@/library/customer.context";
 
-const  CustomerLayout = async ({
-    children,
+const CustomerLayout = async ({
+  children,
 }: Readonly<{
-    children: React.ReactNode;
-}>,) => {
-    const session = await auth()
-    
+  children: React.ReactNode;
+}>) => {
+  const session = await auth();
 
-    return (
-        <CustomerContextProvider>
-            <div style={{ display: "flex" }}>
-                <div className='left-side' style={{ minWidth: 80 }}>
-                    <CustomerSideBar />
-                </div>
-                <div className='right-side' style={{ flex: 1 }}>
-                    <CustomerHeader userName = {session?.user?.name ? session?.user?.name : session?.user?.email}/>
-                    <CustomerContent>
-                        {children}
-                    </CustomerContent>
-                    <AdminFooter />
-                </div>
-            </div>
-        </CustomerContextProvider>
-    )
-}
+  return (
+    <CustomerContextProvider>
+      <div style={{ display: "flex" }}>
+        <div className="left-side">
+          <CustomerSideBar />
+        </div>
+        <div className="right-side" style={{ flex: 1 }}>
+          <CustomerHeader
+            userName={
+              session?.user?.name ? session?.user?.name : session?.user?.email
+            }
+          />
+          <CustomerContent>{children}</CustomerContent>
+          <AdminFooter />
+        </div>
+      </div>
+    </CustomerContextProvider>
+  );
+};
 
-export default CustomerLayout
+export default CustomerLayout;
