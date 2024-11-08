8f815dca4244c56279f43e337b76d2ec9b7bb29d
Quan <phamdinhquan202@gmail.com>
2024-11-08 12:49:07 +0700

    fix bug

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -19,6 +19,7 @@ const Login = () => {
     const { username, password } = values;
     // trigger sign-in
     const res = await authenticate(username, password);
+    console.log(res);
     if (res?.error) {
       // error
       notification.error({
